38ae517a17eade15c511be9515cdf651c84e86c3
Antony <antonydoria@gmail.com>
2015-11-06 16:28:33 +0100

    Tâche n°3 : Introduction des groupes dans les layers

--- src/org/ulco/Layer.java
+++ src/org/ulco/Layer.java
@@ -1,14 +1,12 @@
 package org.ulco;
 
-import java.util.Vector;
-
-public class Layer extends GraphicsObjects {
+public class Layer extends Group {
     public Layer() {
-        m_ID = ++ID.ID;
+        super();
     }
 
     public void init(String json) {
-        String str = json.replaceAll("\\s+", "");
+        String str = json.replaceAll("\\s+","");
         int objectsIndex = str.indexOf("objects");
         int endIndex = str.lastIndexOf("}");
 
@@ -20,57 +18,7 @@ public class Layer extends GraphicsObjects {
     }
 
     public int getObjectNumber() {
-        return size();
-    }
-
-    public int getID() {
-        return m_ID;
-    }
-
-    private void parseObjects(String objectsStr) {
-        while (!objectsStr.isEmpty()) {
-            int separatorIndex = searchSeparator(objectsStr);
-            String objectStr;
-
-            if (separatorIndex == -1) {
-                objectStr = objectsStr;
-            } else {
-                objectStr = objectsStr.substring(0, separatorIndex);
-            }
-
-            add(JSON.parse(objectStr));
-
-            if (separatorIndex == -1) {
-                objectsStr = "";
-            } else {
-                objectsStr = objectsStr.substring(separatorIndex + 1);
-            }
-        }
-    }
-
-    private int searchSeparator(String str) {
-        int index = 0;
-        int level = 0;
-        boolean found = false;
-
-        while (!found && index < str.length()) {
-            if (str.charAt(index) == '{') {
-                ++level;
-                ++index;
-            } else if (str.charAt(index) == '}') {
-                --level;
-                ++index;
-            } else if (str.charAt(index) == ',' && level == 0) {
-                found = true;
-            } else {
-                ++index;
-            }
-        }
-        if (found) {
-            return index;
-        } else {
-            return -1;
-        }
+        return this.size();
     }
 
     public GraphicsObjects select(Point pt, double distance) {
@@ -95,8 +43,12 @@ public class Layer extends GraphicsObjects {
                 str += ", ";
             }
         }
+
+        if (null != group) {
+            str += ", groups : { ";
+            group.toJson();
+        }
+
         return str + " } }";
     }
-
-    private int m_ID;
 }
